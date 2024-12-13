4f38fe076f2fa384915cf6a42e8e2de190579b1f
Zoltan <arsendulyan@yandex.ru>
2024-12-13 15:15:19 +0300

    прикол

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,18 +1,12 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-
 protocol Alive {
     func getName() -> String
     func getAge() -> Int
     func makeSound() -> String
 }
 
-
 class Animal: Alive {
     private let name: String
     private let age: Int
-    
     
     init(name: String, age: Int) {
         self.name = name
@@ -27,27 +21,22 @@ class Animal: Alive {
         return age
     }
     
-
     func makeSound() -> String {
         return ""
     }
 }
 
-
 class Cat: Animal {
     override func makeSound() -> String {
-        return Bool.random() ? "meow" : ""
+        return Bool.random() ? "meow" : "..."
     }
 }
-
 
 class Dog: Animal {
-    
     override func makeSound() -> String {
-        return Bool.random() ? "bark" : ""
+        return Bool.random() ? "bark" : "..."
     }
 }
-
 
 class Person {
     private let name: String
@@ -60,21 +49,23 @@ class Person {
         self.companion = companion
     }
     
-    
-    
     func getCompanionInfo() {
         let companion = companion
+        let sound = companion.makeSound()
+        let soundOutput = sound.isEmpty ? "is silent" : "says '\(sound)'"
         
-        print("\(name)'s companion is \(companion.getName()), age \(companion.getAge()), who says: '\(companion.makeSound())'")
+        print("\(name)'s companion is \(companion.getName()), age \(companion.getAge()), who \(soundOutput).")
     }
 }
 
 
-let person1 = Person(name: "alice", age: 30, companion: catCompanion)
-let person2 = Person(name: "joe", age: 25, companion: dogCompanion)
+let catCompanion = Cat(name: "Megatron", age: 3)
+let dogCompanion = Dog(name: "Bartholomew", age: 5)
 
-let catCompanion = Cat(name: "megatron", age: 3)
-let dogCompanion = Dog(name: "bartholomew", age: 5)
+
+let person1 = Person(name: "Alice", age: 30, companion: catCompanion)
+let person2 = Person(name: "Joe", age: 25, companion: dogCompanion)
+
 
 person1.getCompanionInfo()
 person2.getCompanionInfo()
